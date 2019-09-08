2eded36b4fb10ba0c4b43933d54ed6946c8c8b2b
nohi <thisisnohi@163.com>
2019-09-08 16:04:22 +0800

    修改曲线产品列表查询接口服务名

--- src/api/curve/curve-product-list.js
+++ src/api/curve/curve-product-list.js
@@ -1,10 +1,10 @@
 import request from '@/utils/request-client'
-import { basic_api_market } from '@/api/base-api.js'
+import { basic_api_curve } from '@/api/base-api.js'
 
 // 查询曲线产品列表
 export function queryCurveProductList(data) {
   return request({
-    url: `${basic_api_market}/curveProduct/productList`,
+    url: `${basic_api_curve}/curveProduct/productList`,
     method: 'post',
     data
   })
