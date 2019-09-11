42a1cfb3b3032f71907459ce296748174ed0040d
Your Name <17303846779@163.com>
2019-09-11 20:21:25 +0800

    添加估值强制推荐规则

--- src/views/valuation/rec-mandatory/rec-mandatory-task.vue
+++ src/views/valuation/rec-mandatory/rec-mandatory-task.vue
@@ -1,9 +1,56 @@
 <template>
-  <div class="app-container" />
+  <div class="app-container">
+    <recForceForm
+      ref="refApprovalDialog"
+      :business-id="businessNo"
+      :disabled="disabled"
+    />
+    <div class="button-box-fixed">
+      <el-button type="primary" @click="taskSubmit('02')">审核通过</el-button>
+      <el-button type="primary" @click="taskSubmit('03')">审核拒绝</el-button>
+      <el-button>取 消</el-button>
+    </div>
+  </div>
 </template>
+
 <script>
+import recForceForm from '@/views/valuation/rec-mandatory/rec-mandatory-form.vue'
+import { taskSubmit } from '@/api/valuation/rec-mandatory.js'
 export default {
-  name: 'RecMandatoryTask'
+  name: 'RecForceTask',
+  components: {
+    recForceForm
+  },
+  data() {
+    return {
+      businessNo: '',
+      disabled: true
+    }
+  },
+  beforeMount() {
+    this.businessNo = this.$store.state.task.businessNo
+  },
+  mounted() {
+    this.$store.commit('task/clear')
+  },
+  methods: {
+    taskSubmit(status) {
+      taskSubmit({
+        businessNo: this.businessNo,
+        taskStatus: status,
+        taskOpinions: '',
+        taskType: '01'
+      }).then(response => {
+        this.$message({
+          message: '提交成功！',
+          type: 'success',
+          showClose: true
+        })
+        this.$store.dispatch('homePage/queryTaskList')
+        this.$router.push({ path: '/' })
+      })
+    }
+  }
 }
 </script>
 
