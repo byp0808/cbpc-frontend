c967cf8a97ad72bf5905aaa34ea390e541164be5
Your Name <17303846779@163.com>
2019-10-09 10:05:53 +0800

    查询估值菜单项

---
+++ src/utils/file-request.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { getToken } from '@/utils/auth'
+import uuid from 'uuid/v4'
+
+export function upload({ url, data }) {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_BASE_API,
+    withCredentials: true
+  })
+  data.append('com.version', '1.0.0')
+  data.append('com.globalReqNumber', uuid())
+  data.append('com.sysIsEncrypted', '01')
+  return instance.post(url, data, { transformRequest: [function(data, headers) {
+    if (headers['Content-Type'] === 'multipart/form-data') {
+      return data
+    } else {
+      headers['Content-Type'] = 'application/json'
+    }
+    return JSON.stringify(data)
+  }], headers: { 'Content-Type': 'multipart/form-data', token: getToken() }}).then(response => {
+    const res = response.data
+    console.log('response: ', res)
+    if (res.flag !== 0) {
+      return Promise.reject(new Error(res.msg || 'Error'))
+    } else {
+      return res.data
+    }
+  }).catch(message => {
+    console.log('message: ', message)
+    this.$message.error('上传失败，请联系管理员')
+  })
+}
