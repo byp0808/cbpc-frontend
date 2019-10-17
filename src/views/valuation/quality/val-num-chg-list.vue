2862b369e60c144f4e6226b7371b535161b00280
whustc <wanghaowind@163.com>
2019-10-17 18:46:38 +0800

    VALQC

--- src/views/valuation/quality/val-num-chg-list.vue
+++ src/views/valuation/quality/val-num-chg-list.vue
@@ -7,47 +7,47 @@
       style="width: 100%"
       @selection-change="handleSelectionChange"
     >
-      <el-table-column prop="bondId" label="债券简称" width="120" show-overflow-tooltip>
+      <el-table-column prop="bondNm" label="债券简称" width="120" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.bondNm }}
         </template>
       </el-table-column>
-      <el-table-column prop="productName" label="债券代码" width="150" show-overflow-tooltip>
+      <el-table-column prop="csin" label="债券代码" width="150" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.csin }}
         </template>
       </el-table-column>
-      <el-table-column prop="curvePubType" label="流通场所" width="80" show-overflow-tooltip>
+      <el-table-column prop="market" label="流通场所" width="80" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.market }}
         </template>
       </el-table-column>
-      <el-table-column prop="compDate" label="债券性质" width="100" show-overflow-tooltip>
+      <el-table-column prop="bondNature" label="债券性质" width="100" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.bondNature }}
         </template>
       </el-table-column>
-      <el-table-column prop="batchId" label="待偿期" width="100" show-overflow-tooltip>
+      <el-table-column prop="residualMaturity" label="待偿期" width="100" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.residualMaturity }}
         </template>
       </el-table-column>
-      <el-table-column prop="rateType" label="付息方式" width="100" show-overflow-tooltip>
+      <el-table-column prop="intrstPayMthod" label="付息方式" width="100" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.intrstPayMthod }}
         </template>
       </el-table-column>
-      <el-table-column prop="interestDueFreq" label="是否含权" width="100" show-overflow-tooltip>
+      <el-table-column prop="optionEmbedIncd" label="是否含权" width="100" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.optionEmbedIncd }}
         </template>
       </el-table-column>
-      <el-table-column prop="curveBuildStatus" label="状态" width="150" show-overflow-tooltip>
+      <el-table-column prop="status" label="状态" width="150" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.status }}
         </template>
       </el-table-column>
-      <el-table-column prop="assignName" label="原因" width="100" show-overflow-tooltip>
+      <el-table-column prop="excptRsn" label="原因" width="100" show-overflow-tooltip>
         <template slot-scope="scope">
           {{ scope.row.excptRsn }}
         </template>
@@ -112,7 +112,7 @@ export default {
       this.valNumChgList.compDate = this.taskDay
       this.valNumChgList.batchId = this.orderId
       qryValNumChgQcRpt(this.valNumChgList).then(response => {
-        console.info('qryCurveOverallQcRpt.qryCurveOverallQcRpt...')
+        console.info('qryValNumChgQcRpt.qryValNumChgQcRpt...')
         const { dataList, page } = response
         this.valNumChgList.dataList = dataList
         this.valNumChgList.page = page
