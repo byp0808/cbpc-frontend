7a95dfdbfc4ce3e4806a03a026b93bf6f979c56f
chenshijie <17303846779@163.com>
2019-10-17 10:01:26 +0800

    查询人工估值

--- src/views/valuation/query/people-list.vue
+++ src/views/valuation/query/people-list.vue
@@ -2,7 +2,7 @@
   <div class="box">
     <div>
       <el-table
-        :data="allList"
+        :data="peopleValuationList"
         max-height="300"
         style="width: 100%"
         :header-cell-style="{background:'#f6f6f6'}"
@@ -15,62 +15,67 @@
         <el-table-column align="center" label="全选" type="selection" />
         <el-table-column label="估值日期" align="center">
           <template slot-scope="scope">
-            <span>{{ scope.row.cause }}</span>
+            <span>{{ scope.row.cptDate }}</span>
           </template>
         </el-table-column>
         <el-table-column label="估值批次" align="center">
           <template slot-scope="scope">
-            <span>{{ scope.row.cause }}</span>
+            <span>{{ scope.row.batchId }}</span>
           </template>
         </el-table-column>
         <el-table-column label="操作人" align="center">
           <template slot-scope="scope">
-            <span>{{ scope.row.remark }}</span>
+            <span>{{ scope.row.lastUpdBy }}</span>
           </template>
         </el-table-column>
         <el-table-column label="债券简称" align="center">
           <template slot-scope="scope">
-            <span>{{ scope.row.bondsId }}</span>
+            <span>{{ scope.row.bondId }}</span>
           </template>
         </el-table-column>
         <el-table-column label="债券代码" align="center">
           <template slot-scope="scope">
-            <span>{{ scope.row.bondsId }}</span>
+            <span>{{ scope.row.CSIN }}</span>
           </template>
         </el-table-column>
         <el-table-column label="日间估价全价（元）" align="center" width="160px">
           <template slot-scope="scope">
-            <span>{{ scope.row.filterId }}</span>
+            <span>{{ scope.row.indexResult.fullPriceInterDay }}</span>
           </template>
         </el-table-column>
         <el-table-column label="日间应计利息（元）" align="center" width="160px">
           <template slot-scope="scope">
-            <span>{{ scope.row.filterId }}</span>
+            <span>{{ scope.row.indexResult.accruedInterestInterDay }}</span>
           </template>
         </el-table-column>
         <el-table-column label="估价净价（元）" align="center" width="160px">
           <template slot-scope="scope">
-            <span>{{ scope.row.filterId }}</span>
+            <span>{{ scope.row.indexResult.netPrice }}</span>
           </template>
         </el-table-column>
         <el-table-column label="估价收益率（%）" align="center" width="160px">
           <template slot-scope="scope">
-            <span>{{ scope.row.yield }}</span>
+            <span>{{ scope.row.indexResult.yield }}</span>
           </template>
         </el-table-column>
         <el-table-column label="日终估价全价（元）" align="center" width="160px">
           <template slot-scope="scope">
-            <span>{{ scope.row.filterId }}</span>
+            <span>{{ scope.row.indexResult.fullPriceEndDay }}</span>
           </template>
         </el-table-column>
         <el-table-column label="日终应计利息（元）" align="center" width="160px">
           <template slot-scope="scope">
-            <span>{{ scope.row.filterId }}</span>
+            <span>{{ scope.row.indexResult.accruedInterestEndDay }}</span>
           </template>
         </el-table-column>
         <el-table-column label="可信度" align="center">
           <template slot-scope="scope">
-            <span>{{ scope.row.filterId }}</span>
+            <span>{{ scope.row.reliability }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="长/短" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.resultDirection }}</span>
           </template>
         </el-table-column>
       </el-table>
@@ -80,25 +85,24 @@
 <script>
 
 export default {
-  name: 'AssetList',
+  name: 'PeopleList',
   components: {
 
   },
-  props: {
-    allList: {
-      type: Array,
-      default: function() {
-        return []
-      }
-    }
-  },
-  //   props: ['allList'],
   data() {
     return {
       ruleSetFormVisible: false,
       taskRangeId: '',
       selectionList: [],
       list: []
+    }
+  },
+  computed: {
+    // 人工估值列表
+    peopleValuationList: {
+      get() {
+        return this.$store.state.queryValuationScheme.peopleValuationList
+      }
     }
   },
   methods: {
