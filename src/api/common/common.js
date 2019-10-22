db1d1ce392b4b13c1a4e3e0e27a9441efaac1bee
lee <liweidong@scfsoft.com>
2019-10-22 18:16:59 +0800

    bug 修改 文件上传接口

--- src/api/common/common.js
+++ src/api/common/common.js
@@ -1,5 +1,6 @@
 import request from '@/utils/app-request'
 import { basic_api_portal } from '@/api/base-api.js'
+import { upload } from '@/utils/file-request.js'
 
 // 查字典
 export function queryDictList(data) {
@@ -7,5 +8,18 @@ export function queryDictList(data) {
     url: `${basic_api_portal}/sys/sys-code/dicts`,
     method: 'post',
     data
+  })
+}
+
+// 文件上传
+export function uploadFile(data) {
+  const form = new FormData()
+  form.append('file', data.file)
+  form.append('zone', 'pek3b')
+  form.append('bucketName', 'leewd-test')
+  return upload({
+    url: `${basic_api_portal}/pi-sys/common/attach/upload`,
+    method: 'post',
+    data: form
   })
 }
