df290e746d665aa46a397679872a38ebbae70a9d
whustc <wanghaowind@163.com>
2019-11-02 14:23:24 +0800

    QCPARM

--- src/views/valuation/quality/val-param-set-form.vue
+++ src/views/valuation/quality/val-param-set-form.vue
@@ -77,7 +77,7 @@ export default {
     handleClick(tab, event) {
       console.log(tab, event)
       console.info('val-param-set-form.vue handleClick:')
-      this.activeName = this.orderList[0].id
+      // this.activeName = this.orderList[0].id
       this.qryValQcParm()
     },
 
