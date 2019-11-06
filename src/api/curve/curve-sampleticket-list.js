e10d85084faef7955e45de86ef2cd01b13bf1704
dinglonghai <thisisnohi@163.com>
2019-11-06 18:06:22 +0800

    增加下载曲线样本券功能

--- src/api/curve/curve-sampleticket-list.js
+++ src/api/curve/curve-sampleticket-list.js
@@ -1,5 +1,6 @@
 import request from '@/utils/app-request'
 import { basic_api_curve } from '@/api/base-api.js'
+import { downloadFile } from '@/utils/file-request'
 
 export function queryCurveSampleBondsList(data) {
   return request({
@@ -7,4 +8,8 @@ export function queryCurveSampleBondsList(data) {
     method: 'post',
     data
   })
+}
+
+export function downloadSample(data) {
+  downloadFile(`${process.env.VUE_APP_BASE_API}${basic_api_curve}` + '/curveSampleQuery/download', data)
 }
